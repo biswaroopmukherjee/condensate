15341d459c7e72960792a3839d1ddac37d6cb6b3
Biswaroop Mukherjee <mail.biswaroop@gmail.com>
2020-04-10 02:22:44 -0400

    Cleanup in progress

--- notebooks/evolution/evolve.py
+++
@@ -1,5 +0,0 @@
-from condensate.core import Potential, Evolve
-
-class Evolve:
-    def __init__(potential, dt, steps, show=True, save=False, frame='lab', Omega=0)
-        self.potential = potential
--- start_condensate.sh
+++ start_condensate.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 # This is the startup script for condensate.
 
-if [ $# = 1 ]
-then
-    if [ $1 == "docker" ]
-    then
-        DOCKER_BUILDKIT=1 docker build --tag=gp .
-        xhost +local:docker
+DOCKER_BUILDKIT=1 docker build --tag=gp .
+xhost +local:docker
 
-        docker container rm condensate 
+docker container rm condensate 
+docker system prune -f
 
-        docker run -it \
-            --name condensate \
-            --env="DISPLAY" \
-            --env="QT_X11_NO_MITSHM=1" \
-            --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-            --runtime=nvidia \
-            --volume $(pwd)/notebooks:/gp/notebooks \
-            -p 8888:8888\
-            gp 
-    fi
-else
-    cd build
-    cmake ..
-    make
-    ./gp
-fi
+docker run -it \
+    --name condensate \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --runtime=nvidia \
+    --volume $(pwd)/notebooks:/gp/notebooks \
+    -p 8888:8888\
+    gp 
